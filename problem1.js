e5a0e7694600c301729ea3b1a60f364c1c1fbdb9
Shubh-am1999 <shubhambharti731@gmail.com>
2022-09-06 16:43:52 +0530

    iw

---
+++ problem1.js
@@ -0,0 +1,8 @@
+let a = "678";
+let b = "700";
+if (a>b){
+  console.log("Discount Applied");
+
+}
+else(a<b) 
+  console.log("No Discount");
---
+++ problem2.js
@@ -0,0 +1,11 @@
+let a = 700;
+let b = 600
+
+if (a>b){
+  console.log("Give Him Discount Of 10 %");
+  
+}
+else
+{
+console.log("No Discount");
+}
